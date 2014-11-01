e51b57794a928e69f1417f2644a024fede9471dd
Hubert Kunnemeyer <hubiedoo517@gmail.com>
2014-11-01 12:24:28 -0400

    - Adding AWSiOS SDK for use with Amazon Web Services - Updating podfile

---
+++ BeaconMeetings/RoomCloudManager.h
@@ -0,0 +1,13 @@
+//
+//  RoomCloudManager.h
+//  BeaconMeetings
+//
+//  Created by Hubert Kunnemeyer on 11/1/14.
+//  Copyright (c) 2014 WebMD. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RoomCloudManager : NSObject
+
+@end
---
+++ BeaconMeetings/RoomCloudManager.m
@@ -0,0 +1,13 @@
+//
+//  RoomCloudManager.m
+//  BeaconMeetings
+//
+//  Created by Hubert Kunnemeyer on 11/1/14.
+//  Copyright (c) 2014 WebMD. All rights reserved.
+//
+
+#import "RoomCloudManager.h"
+
+@implementation RoomCloudManager
+
+@end
